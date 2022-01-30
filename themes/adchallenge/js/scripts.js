a542289c96ca38fa4d3c6c885224472ea3646c1a
Enes Gutic <25060614+enesgutic@users.noreply.github.com>
2022-01-30 17:20:42 +0100

    Remove header opacity on scroll

--- themes/adchallenge/js/scripts.js
+++ themes/adchallenge/js/scripts.js
@@ -5,4 +5,22 @@ document.getElementById('block-adchallenge-content').setAttribute('id', 'challen
 const element = document.querySelector('.site-slogan');
 element.innerHTML = '<strong>Music</strong>Theme';
 
+// Insert head script with jquery cdn
+var my_awesome_script = document.createElement('script');
+my_awesome_script.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
+document.head.appendChild(my_awesome_script);
 
+jQuery(function() {
+
+  jQuery(document).ready(function() {
+    jQuery(window).scroll(function(){
+        if ($(this).scrollTop() > 5) {
+           $('header').addClass('dark');
+        } else {
+           $('header').removeClass('dark');
+        }
+    });
+  });
+
+
+});
